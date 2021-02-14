911b6f5e5b5391451b5a12553e49b68d26ddf225
ozkilic <ozkilicyunusemre@gmail.com>
2021-02-15 00:43:17 +0200

    fixed port

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { ApolloServer, PubSub } = require('apollo-server')
 const mongoose = require('mongoose')
 
-const typeDefs = require('./graphql/typedefs')
+const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js')
 
 const pubsub = new PubSub()
 
-const PORT = process.env.PORT || 500
+const PORT = process.env.PORT || 5000
 
 const server = new ApolloServer({
   typeDefs,
